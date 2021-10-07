383e085a4d186032066d7fe49c445850dae31871
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2021-10-07 12:15:24 +0200

    ✨ Construct file header tokens from version

--- packages/core/model/version.go
+++ packages/core/model/version.go
@@ -10,3 +10,7 @@ type Version struct {
 func (v Version) String() string {
 	return fmt.Sprintf("%0d.%0d", v.Major, v.Minor)
 }
+
+func (v Version) AsFileHeaderToken() []byte {
+	return []byte(fmt.Sprintf("%sPDF-%0d.%0d", "%", v.Major, v.Minor))
+}
